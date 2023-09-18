6b442800ee439d77c042a27fb731a7372a9f9c01
Iamneo <techadmin@iamneo.ai>
2023-09-18 09:56:49 +0000

    Backup Commit

---
+++ Week2Day1_4/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2Day1_4.Models
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Category { get; set; }
+        public decimal BiddingPrice { get; set; }
+    }
+}
